440a19e12887da0dbd8c615ab63c3ae9a3c48916
fborquez <fborquez@CL-NBDLI7-44.tecnofarma.cl>
2020-05-26 00:00:28 -0400

    Primer Commit

---
+++ WildJSF/src/cl/usm/SaludoBean.java
@@ -0,0 +1,19 @@
+package cl.usm;
+
+public class SaludoBean {
+	
+	private String bienvenida;
+	
+	public SaludoBean() {
+		this.bienvenida = "Hola Coronavirus!";
+	}
+
+	public String getBienvenida() {
+		return bienvenida;
+	}
+
+	public void setBienvenida(String bienvenida) {
+		this.bienvenida = bienvenida;
+	}
+
+}
